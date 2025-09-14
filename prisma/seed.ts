a7884caff9354262afe9d976db36049c1234c54f
DAINI <apdk_4869@yahoo.co.jp>
2025-09-15 07:41:09 +0900

    The seed command has been executed.

---
+++ prisma/seed.ts
@@ -0,0 +1,24 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  await prisma.vods.createMany({
+    data: [
+      { code: 'NETFLIX',     name: 'Netflix' },
+      { code: 'PRIME_VIDEO', name: 'Prime Video' },
+      { code: 'DISNEY_PLUS', name: 'Disney+' },
+    ],
+    skipDuplicates: true, // すでに登録されていたら無視
+  });
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
